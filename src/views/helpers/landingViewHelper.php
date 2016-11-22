9c1951495a4034663730359830d65f8900ec547d
Fion <fionleong94@gmail.com>
2016-11-21 23:25:59 -0800

    Updated some code

--- src/views/helpers/landingViewHelper.php
+++ src/views/helpers/landingViewHelper.php
@@ -9,18 +9,18 @@ class landingViewHelper extends Helper {
     ?>
     <h1>PasteChart</h1>
     <h3>Share your data in charts!</h3>
-    <form action="./index.php?c=" method="post">
+    <form action="./index.php?" method="post">
       <input type="hidden" name="c" value="chart"/>
       <input type="hidden" name="a" value="show"/>
       <label for="chartTitle">Chart Title: </label><br>
       <input type="text" name="chartTitle" id="chartTitle" size="48"/><br></br>
       <label for="data">Data: </label><br>
-      <textarea name="data" id="data" rows="10" cols="50" maxlength="80" placeholder="The first coordinate should represent a text label (can involve numbers but can't involve a comma), the remaining coordinates should represent values that correspond to that text label from up to 5 sources. Example: Jan,50,12,7.1,8,94"></textarea><br></br>
+      <textarea name="data" id="data" rows="10" cols="50" placeholder="The first coordinate should represent a text label (can involve numbers but can't involve a comma), the remaining coordinates should represent values that correspond to that text label from up to 5 sources. Example: Jan,50,12,7.1,8,94"></textarea><br></br>
       <button type="submit" onclick="return ">Share</button>
     </form>
     <script src="chart.js"></script>
     <script>
-    scroll_messages();
+    
     </script>
     <?php
   }
